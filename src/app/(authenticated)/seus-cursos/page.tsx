d28539764209f12c0f7cc6cdb2a60f19cfb658b2
curtytech <curtytech@gmail.com>
2024-06-17 19:05:07 -0300

    updating seus-cursos

--- src/app/(authenticated)/seus-cursos/page.tsx
+++ src/app/(authenticated)/seus-cursos/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import CourseCardItem from "../../../components/CourseCardItem";
 import * as Icon from "@phosphor-icons/react/dist/ssr";
 import { Button } from "../../../components/ui/button";
+import Hero from "../../../components/Hero";
+import CourseCardInfo from "@/components/CourseCardInfo";
 
 export default function SeusCursos() {
     return (
-        <div className="bg-black">
+        <div className="w-full px-10 mt-5">
             <div className="container mx-auto p-4">
-                <p>header</p>
+                <Hero />
             </div>
 
             <div className="flex flex-row p-4">
@@ -31,54 +33,23 @@ export default function SeusCursos() {
                 </div>
             </div> */}
 
-            <div className="w-full p-4 gap-3 flex flex-row justify-between">
-                <div className="w-3/4 flex flex-col gap-4">
-                    <div className="flex flex-row gap-4">
-                        <div className="flex flex-row bg-gray-dark rounded w-1/5 rounded-lg overflow-hidden mt-10 relative before:absolute before:z-20 before:bg-black before:h-full before:w-full before:bg-gradient-to-tr before:from-black before:to-black-600 before:opacity-80">
-                            <img src="https://picsum.photos/400/400" className="absolute z-10 object-cover h-80 w-full h-full" alt="" />
-                            <div className="flex flex-col justify-end relative z-20 p-10 h-full w-1/2 justify-end">                            
-                                <h2 className="text-white text-3xl font-semibold">Luis Carvalho</h2>
-                                <h3 className="text-white text-xl">Docteur Irako Andriamahasoa Ammi</h3>
-                                <div className="flex flex-row justify-between items-center gap-3 mt-2">
-                                    <p className="text-xs text-gray-light mt-2 text-xs">00:55:21</p>
-                                    <Button className="px-2 py-1 bg-red">
-                                        <Icon.Heart size={17} className="text-white" />
-                                    </Button>
-                                    <Button className="px-2 py-1 bg-gray-light ">
-                                        <Icon.Play size={17} className="text-white" />
-                                    </Button>
-                                </div>
-                            </div>                            
-                        </div>
-                    </div>
-                </div>
-                <div className="w-1/4 flex flex-col gap-4 ">
-                    <img src="https://picsum.photos/400/200" alt="" />
-
-                    <div className="flex flex-row -mt-8 mx-1 justify-between items-center">
-                        <div className="flex flex-col">
-                            <h6 className="mx-2 uppercase text-white text-xs font-thin text-white">Nome do</h6>
-                            <h3 className="mx-2 -mt-2 uppercase text-lg text-white font-bold">Curso</h3>
-                        </div>
-                        <div className="flex flex-row items-center">
-                            <h6 className="mt-1 text-xs text-gray-light">Progresso:</h6>
-                            <h6 className="text-xl text-green-tridar">35%</h6>
-                        </div>
-                    </div>
-                    <div className="flex flex-row justify-between items-center text-gray-light">
-                        <div className="flex flex-row gap-1">
-                            <Icon.Clock size={17} className="text-gray-light" />
-                            <p className="text-gray-light text-xs">Duração do Curso</p>
-                        </div>
-                        <div className="flex flex-row gap-1">
-                            <p className="text-gray-light text-xs">120h</p>
-                        </div>
+            <div className="flex flex-col md:flex-row justify-between p-4 space-y-4 md:space-y-0 md:space-x-4">
+                <div className="flex w-full md:w-3/4">
+                    <div className="flex flex-row">
+                        <img src="https://picsum.photos/400/100" alt="" />
                     </div>
 
-                    <Button className="px-2 py-1 bg-green-tridar text-black text-xs :hover:bg-green-800"> Entrar no Curso </Button>
-                    <Button className="px-2 py-1 bg-gray-dark text-xs text-white"> Aulas Salvas </Button>
+                    <div className="flex flex-col p-4">
+                        <h2 className="text-white text-xl font-thin uppercase">Nome do</h2>
+                        <h3 className="text-white text-3xl font-bold uppercase">Curso</h3>
+
+                        <p className="text-white text-xs mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+
                 </div>
+                <CourseCardInfo />
             </div>
+
         </div >
     );
 }
--- src/app/(authenticated)/trade-tv/page.tsx
+++ src/app/(authenticated)/trade-tv/page.tsx
@@ -26,6 +26,27 @@ export default function TradeTv() {
                         allowFullScreen
                         className="w-full h-full"
                     ></iframe>
+
+                    <div className="flex flex-row justify-between p-4">
+                        <h2 className="text-white text-xl mb-4 font-bold"> Live Trade Tv</h2>
+                        <div className="flex gap-3">
+                            <Icon.Heart className="bg-gray-dark text-white p-2 rounded" size={35} />
+                            <Icon.ArrowLeft className="bg-gray-dark text-white p-2 rounded" size={35} />
+                            <Icon.ArrowRight className="bg-gray-dark text-white p-2 rounded" size={35} />
+                        </div>
+                    </div>
+                    <div className="flex flex-row mb-4 ">
+                        <div className="mx-1 py-2 px-4 rounded bg-gray-dark text-green-tridar text-sm"> Descrição </div>
+                        <span className="flex items-center mx-1 py-2 px-4 rounded text-white text-sm bg-gray-dark"> <Icon.User className=" text-white mr-2" size={20} /> Seguir </span>
+
+                    </div>
+
+                    <div className="flex flex-row">
+                        <div className="bg-gray-dark p-4 ">
+                            <h6 className="text-white">Descrição</h6>
+                            <p className="text-gray-light mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui praesentium tenetur natus repellendus ipsa ipsam eligendi, similique commodi. Ea iure delectus accusantium totam quo repellendus soluta eaque labore vero dolorem!</p>
+                        </div>
+                    </div >
                 </div>
 
                 <div className="w-full md:w-1/4 flex flex-col">
@@ -53,7 +74,7 @@ export default function TradeTv() {
                         <input className="focus:outline-none w-full rounded-r rounded-lg p-2 text-sm bg-gray-dark text-gray-light" placeholder="Digite sua mensagem..." />
                         <button className="bg-white text-white rounded-r p-3">
                             <Icon.PaperPlaneTilt size={17} color="black" />
-                        </button>                      
+                        </button>
                     </div>
 
 
@@ -68,32 +89,28 @@ export default function TradeTv() {
                         </div>
                     </div>
 
-                    <h6 className="text-white mt-4"> <Dot/> Lives Relacionadas</h6>
+                    <h6 className="text-white mt-4"> <Dot /> Lives Relacionadas</h6>
 
+                    <div className="flex flex-col mt-4">
+                        <div className="flex flex-row justify-between bg-gray-dark mt-4 w-full ">
+                            <div className="flex w-3/8 ">
+                                <img className="" src="https://picsum.photos/350/50" alt="" />
+                            </div>
+                            <div className="flex flex-col w-5/8 p-4">
+                                <p className="text-gray-light text-xs mt-3">Luis Carvalho</p>
+                                <h6 className="text-white text-sm mt-3">Análise de WINFUT - IBOV - DOLV23 + ATIVOS</h6>
+
+                                <p className="text-gray-light text-xs mt-3">Postado há 8 dias • 10k visualizações</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+
 
             </div>
 
-            <div className="flex flex-row justify-between p-4 w-3/4">
-                <h2 className="text-white text-xl mb-4 font-bold"> Live Trade Tv</h2>
-                <div className="flex gap-3">
-                    <Icon.Heart className="bg-gray-dark text-white p-2 rounded" size={35} />
-                    <Icon.ArrowLeft className="bg-gray-dark text-white p-2 rounded" size={35} />
-                    <Icon.ArrowRight className="bg-gray-dark text-white p-2 rounded" size={35} />
-                </div>
-            </div>
-            <div className="flex flex-row mb-4">
-                <div className="mx-1 py-2 px-4 rounded bg-gray-dark text-green-tridar text-sm"> Descrição </div>
-                <span className="flex items-center mx-1 py-2 px-4 rounded text-white text-sm bg-gray-dark"> <Icon.User className=" text-white mr-2" size={20} /> Seguir </span>
 
-            </div>
-
-            <div className="flex flex-row">
-                <div className="w-3/4 bg-gray-dark p-4">
-                    <h6 className="text-white">Descrição</h6>
-                    <p className="text-gray-light mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui praesentium tenetur natus repellendus ipsa ipsam eligendi, similique commodi. Ea iure delectus accusantium totam quo repellendus soluta eaque labore vero dolorem!</p>
-                </div>
-            </div >
         </div >
     );
 }
